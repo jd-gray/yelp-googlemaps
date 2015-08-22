ba4e2169c214f1e1e30a26113cd8f9c553d8b8f4
Jared Gray <jd.gray@yahoo.com>
2015-08-22 16:36:30 -0700

    pulling results from yelp

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if params[:term] != nil && params[:city] != nil
+      parameters = { term: params[:term], limit: 10 }
+      @response = Yelp.client.search(params[:city], parameters)
+    end
   end
+
 end
